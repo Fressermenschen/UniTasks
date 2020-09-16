e398a93d6b3a3239eb0f154ee299ad234602050f
Fressermenschen <55920302+Fressermenschen@users.noreply.github.com>
2020-09-16 19:48:59 +0300

    Update ex2.sh

--- ex2.sh
+++ ex2.sh
@@ -3,9 +3,9 @@ lockfile -r 0 /tmp/the.lock || exit 1
 printf '0\n' > ex2.txt
 for i in {0..1000}
 do
-OUT=$(tail -n 1 < ex2.txt)
-((OUT++)) 
-echo "$OUT" >> ex2.txt //critical region
+OUT=$(tail -n 1 < ex2.txt)    //critical region
+((OUT++))                     //critical region
+echo "$OUT" >> ex2.txt        //critical region
 done
 rm -f /tmp/the.lock
 
