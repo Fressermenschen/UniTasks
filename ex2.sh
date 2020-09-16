dccce38fc7d3d29bdd1ac0008d85215533adfe02
Fressermenschen <55920302+Fressermenschen@users.noreply.github.com>
2020-09-16 18:50:48 +0300

    Update ex2.sh

--- ex2.sh
+++ ex2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+lockfile -r 0 /tmp/the.lock || exit 1
 printf '0\n' > ex2.txt
-while(true)
+for i in {0..1000}
 do
 OUT=$(tail -n 1 < ex2.txt)
 ((OUT++)) //critical region
 echo "$OUT" >> ex2.txt
 done
+rm -f /tmp/the.lock
 
 //race condition manifested starting from the fist appended digit
+//the race condition is prevented by locking the file< thus the the script will not run if it is already running
