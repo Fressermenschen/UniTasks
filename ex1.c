d93e7170f7ba2d7ccfcb0503b6415ff3cc79d3f5
Fressermenschen <55920302+Fressermenschen@users.noreply.github.com>
2020-10-28 19:09:19 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -9,34 +9,23 @@
 #include <sys/mman.h>
 #include <string.h>
 
-void main()
-{
+void main() {
     const char *text = "This is a nice day";
-
     const char *filepath = "../week11/ex1.txt";
-
     int fd = open(filepath, O_RDWR);
-
-    if (fd == -1)
-    {
+    if (fd == -1) {
         perror("Error opening file for writing");
         exit(EXIT_FAILURE);
     }
-
+    
     size_t textsize = strlen(text) + 1;
-
     lseek(fd, textsize-1, SEEK_SET);
-
     write(fd, "", 1);
-
     char *map = mmap(0, textsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
     for (size_t i = 0; i < textsize; i++)
         map[i] = text[i];
-
-    msync(map, textsize, MS_SYNC);
-
-    munmap(map, textsize);
     
+    msync(map, textsize, MS_SYNC);
+    munmap(map, textsize);
     close(fd);
 }
