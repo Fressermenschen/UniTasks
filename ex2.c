71011bb3c98758b5e7f1dd06208836297f417064
Fressermenschen <55920302+Fressermenschen@users.noreply.github.com>
2020-10-28 18:09:32 +0300

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,18 @@
+//I guess no comment is required as the program will do everything by itself
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+     printf("H");
+     sleep(1);
+     printf("e");
+     sleep(1);
+     printf("l");
+     sleep(1);
+     printf("l");
+     sleep(1);
+     printf("o");
+     sleep(1);
+     fflush(stdout);
+}
